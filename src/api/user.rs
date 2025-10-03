211ce176deb2adac72f35e5a0bb8efdbeb4d8182
Nf_eyebrows <xiaolizi2580@qq.com>
2025-10-03 13:36:04 +0800

    完成分页查询

--- src/api/user.rs
+++ src/api/user.rs
@@ -4,25 +4,55 @@ use axum::response::IntoResponse;
 use crate::app::AppState;
 use crate::entity::prelude::SysUser;
 use sea_orm::prelude::*;
+use sea_orm::{QueryOrder, QueryTrait, Condition};
+use serde::Deserialize;
+use crate::common::{Page, PaginationParams};
 use crate::entity::prelude::*;
 use crate::entity::sys_user;
 use crate::error::ApiResult;
 use crate::response::ApiResponse;
+use axum::extract::Query;
 
 pub fn create_user() -> Router<AppState>
 {
     Router::new()
-        .route("/", routing::get(query_users))
+        .route("/", routing::get(find_page))
 }
 
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct UserQueryParams
+{
+    keyword: Option<String>,
+    #[serde(flatten)]
+    pagination: PaginationParams
+}
 
 #[debug_handler]
-async fn query_users(State(AppState{ db }): State<AppState>) -> ApiResult<ApiResponse<Vec<sys_user::Model>>>
+async fn find_page(
+    State(AppState{ db }): State<AppState>,
+    Query(UserQueryParams
+    {
+              keyword,
+              pagination,
+    }): Query<UserQueryParams>
+) -> ApiResult<ApiResponse<Page<sys_user::Model>>>
 {
-    let users = SysUser::find()
-        // .filter(Column::Gender.eq("男"))
-        .all(&db)
-        .await.unwrap();
+    let paginator = SysUser::find()
+        .apply_if(keyword.as_ref(), |query, keyword| {
+            query.filter(
+                Condition::any()
+                    .add(sys_user::Column::Name.contains(keyword))
+                    .add(sys_user::Column::Account.contains(keyword))
+            )
+        })
+        .order_by_desc(sys_user::Column::CreatedAt)
+        .paginate(&db, pagination.size);
 
-    Ok(ApiResponse::ok("ok", Some(users)))
+    let total = paginator.num_items().await?;
+    let items = paginator.fetch_page(pagination.page - 1).await?;
+
+    let page = Page::from_pagination(pagination, total, items);
+
+    Ok(ApiResponse::ok("ok", Some(page)))
 }
--- src/server.rs
+++ src/server.rs
@@ -1,11 +1,18 @@
 use crate::app::AppState;
 use crate::config::ServerConfig;
-use axum::extract::State;
-use axum::handler::Handler;
+use crate::Latency::LatencyOnResponse;
+use axum::extract::Request;
+use axum::extract::DefaultBodyLimit;
 use axum::Router;
-use config::Config;
+use bytesize::ByteSize;
 use std::net::SocketAddr;
+use std::time::Duration;
 use tokio::net::TcpListener;
+use tower_http::cors;
+use tower_http::cors::CorsLayer;
+use tower_http::normalize_path::NormalizePathLayer;
+use tower_http::timeout::TimeoutLayer;
+use tower_http::trace::TraceLayer;
 
 pub struct Server
 {
@@ -36,8 +43,40 @@ impl Server
 
     fn build_router(&self, state: AppState, router: Router<AppState >) -> Router
     {
+
+        let timeout = TimeoutLayer::new(Duration::from_secs(120));
+        let body_limit = DefaultBodyLimit::max(
+            ByteSize::mib(10).as_u64() as usize
+        );
+        let cors = CorsLayer::new()
+            .allow_origin(cors::Any)
+            .allow_methods(cors::Any)
+            .allow_headers(cors::Any)
+            .allow_credentials(false)
+            .max_age(Duration::from_secs(3600 * 12));
+
+
+        let tracing = TraceLayer::new_for_http()
+            .make_span_with(|request: &Request| {
+                let method = request.method();
+                let path = request.uri().path();
+                let id = xid::new();
+
+                tracing::info_span!("HTTP REQUEST",id = %id, method = %method, path = %path )
+            })
+            .on_request(())
+            .on_failure(())
+            .on_response(LatencyOnResponse);
+
+        let normalize_path = NormalizePathLayer::trim_trailing_slash();//去掉路径末尾的斜线
+
         Router::new()
             .merge(router)
+            .layer(timeout)
+            .layer(body_limit)
+            .layer(tracing)
+            .layer(cors)
+            .layer(normalize_path)
             .with_state(state)
     }
 
